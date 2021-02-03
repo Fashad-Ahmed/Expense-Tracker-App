bc5aa6cca4f9c9fd76809f9eb4d59da0f91040ed
Fashad-Ahmed <fashad.ahmed20@gmail.com>
2021-02-04 00:46:24 +0500

    Basic components commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { Header } from './components/Header';
+import { Balance } from './components/Balance';
+import { accountSummary } from './components/accountSummary';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <div className='container'>
+        <Balance />
+
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/components/accountSummary.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+export const accountSummary = () => {
+    return (
+        <div className='in-exp-con'>
+            <div>
+                <h4>Income</h4>
+                <p className='mon-pl'>
+                    +$0.00
+                </p>
+            </div>
+            <div>
+                <h4>Expense</h4>
+                <p className='mon-min'>
+                    -$0.00
+            </p>
+            </div>
+        </div>
+
+
+    )
+}
